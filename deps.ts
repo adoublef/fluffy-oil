74723a230c04015466568b70aeafa754039e300a
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-09 08:54:24 +0100

    update cache

--- deps.ts
+++ deps.ts
@@ -1,1 +1,8 @@
+/* std */
 export { serveDir } from "$std/http/file_server.ts";
+/* hono */
+export { Hono } from "$hono/mod.ts";
+export type { Handler } from "$hono/mod.ts";
+export { etag, html, cache } from "$hono/middleware.ts";
+export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
+export type { HtmlEscapedString } from "$hono/utils/html.ts";
