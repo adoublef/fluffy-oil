20092dfd63dd8acda4ff7e157967f8c627929b27
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-10-18 12:12:15 +0100

    edit deps file

--- deps.ts
+++ deps.ts
@@ -1,8 +1,17 @@
-/* std */
+// std
 export { serveDir } from "$std/http/file_server.ts";
-/* hono */
+export { extname } from "$std/path/extname.ts";
+export { contentType } from "$std/media_types/content_type.ts";
+export { join } from "$std/path/mod.ts";
+export { extract } from "$std/front_matter/any.ts";
+// adoublef
+export { serve } from "$x/serve.ts";
+// hono
 export { Hono } from "$hono/mod.ts";
 export type { Handler } from "$hono/mod.ts";
-export { etag, html, cache, cors } from "$hono/middleware.ts";
+export { cache, cors, etag, html, memo, raw } from "$hono/middleware.ts";
 export { serveStatic } from "$hono/adapter/deno/serve-static.ts";
 export type { HtmlEscapedString } from "$hono/utils/html.ts";
+// gfm
+// NOTE -- Custom Render in next release https://github.com/denoland/deno-gfm/pull/80
+export { render as markdown } from "$gfm";
